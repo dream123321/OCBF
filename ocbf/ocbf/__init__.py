a96d66993add5ec97b10fca978613dd29a62f527
huang-jing <137173735+dream123321@users.noreply.github.com>
2026-04-14 10:22:18 +0800

    Delete ocbf directory

--- ocbf/ocbf/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bootstrap import WorkspaceBootstrapper
-from .generation import GenerationRunner
-from .reduce import OCBFReducer
-
-__all__ = ["GenerationRunner", "OCBFReducer", "WorkspaceBootstrapper"]
